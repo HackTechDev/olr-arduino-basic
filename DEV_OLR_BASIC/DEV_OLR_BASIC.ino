972a28f02c7b9b99d7b4a6f2d1cf3d707b0a04c0
Singular Devices <gbarbarov@singulardevices.com>
2019-10-15 22:59:08 +0000

    Add basic config with buttons RED , push on reset to activate gravity, realease after beep to view LED zone ramp active

--- DEV_OLR_BASIC/DEV_OLR_BASIC.ino
+++ DEV_OLR_BASIC/DEV_OLR_BASIC.ino
@@ -34,10 +34,21 @@
 #define PIN_AUDIO      3   // through CAP 2uf to speaker 8 ohms
 
 
+#define INI_RAMP 80
+#define MED_RAMP 90
+#define END_RAMP 100
+#define HIGH_RAMP 12
+bool ENABLE_RAMP=0;
+bool VIEW_RAMP=0;
+
 int NPIXELS=MAXLED; // leds on track
+int cont_print=0;
 
 #define COLOR1   Color(255,0,0)
 #define COLOR2   Color(0,255,0)
+
+#define COLOR1_tail   Color(i*3,0,0)
+#define COLOR2_tail   Color(0,i*3,0)
 
 int win_music[] = {
   2637, 2637, 0, 2637, 
@@ -71,6 +82,7 @@ byte flag_sw2=0;
 byte draworder=0;
  
 unsigned long timestamp=0;
+char txbuff[64];
 
 Adafruit_NeoPixel track = Adafruit_NeoPixel(MAXLED, PIN_LED, NEO_GRB + NEO_KHZ800);
 
@@ -97,13 +109,22 @@ void setup() {
   pinMode(PIN_P2,INPUT_PULLUP);  
 
   if ((digitalRead(PIN_P1)==0)) //push switch 1 on reset for activate physics
-  {
-    set_ramp(12,90,100,110);    // ramp centred in LED 100 with 10 led fordward and 10 backguard 
-    for(int i=0;i<NPIXELS;i++){track.setPixelColor(i, track.Color(0,0,(127-gravity_map[i])/8) );};
-    track.show();
+  {ENABLE_RAMP=1;
+   set_ramp(HIGH_RAMP,INI_RAMP,MED_RAMP,END_RAMP); 
+   for(int i=0;i<(MED_RAMP-INI_RAMP);i++){track.setPixelColor(INI_RAMP+i, track.Color(24+i*4,0,24+i*4) );};  
+   for(int i=0;i<(END_RAMP-MED_RAMP);i++){track.setPixelColor(END_RAMP-i, track.Color(24+i*4,0,24+i*4) );};                                           
+   track.show();
+   delay(1000);
+   tone(PIN_AUDIO,500);delay(500);noTone(PIN_AUDIO);delay(500);
+   if ((digitalRead(PIN_P1)==0)) {VIEW_RAMP=1;} // if retain push switch 1 set view ramp
+   else {for(int i=0;i<NPIXELS;i++){track.setPixelColor(i, track.Color(0,0,0));};
+         track.show();
+         VIEW_RAMP=0;  
+        }; 
+   
   };
 
-  if ((digitalRead(PIN_P2)==0)) {delay(1000); tone(PIN_AUDIO,1000);delay(500);noTone(PIN_AUDIO);if ((digitalRead(PIN_P2)==1)) SMOTOR=1;} //push switch 2 until a tone beep on reset for activate magic FX  ;-)
+  if ((digitalRead(PIN_P2)==0)) {delay(1000); tone(PIN_AUDIO,1000);delay(500);noTone(PIN_AUDIO);delay(500);if ((digitalRead(PIN_P2)==1)) SMOTOR=1;} //push switch 2 until a tone beep on reset for activate magic FX  ;-)
   
   start_race();    
 }
@@ -141,14 +162,48 @@ void start_race(){Serial.println("r4");
 void winner_fx(byte w) {
                int msize = sizeof(win_music) / sizeof(int);
                for (int note = 0; note < msize; note++) {
-               tone(PIN_AUDIO, win_music[note]/(3-w),200);
+               if (SMOTOR==1) {tone(PIN_AUDIO, win_music[note]/(3-w),200);} else {tone(PIN_AUDIO, win_music[note],200);};
                delay(230);
-               noTone(PIN_AUDIO);
+               noTone(PIN_AUDIO);}                                             
+              };
 
+
+int get_relative_position1( void ) {
+    enum{
+      MIN_RPOS = 0,
+      MAX_RPOS = 99,
+    };    
+    int trackdist = 0;
+    int pos = 0;  
+    trackdist = (int)dist1 % NPIXELS;
+    pos = map(trackdist, 0, NPIXELS -1, MIN_RPOS, MAX_RPOS);    
+    return pos;
 }
 
-                                               
-              };
+int get_relative_position2( void ) {
+    enum{
+      MIN_RPOS = 0,
+      MAX_RPOS = 99,
+    };    
+    int trackdist = 0;
+    int pos = 0;  
+    trackdist = (int)dist2 % NPIXELS;
+    pos = map(trackdist, 0, NPIXELS -1, MIN_RPOS, MAX_RPOS);    
+    return pos;
+}
+
+void print_cars_position( void ) {
+ int  rpos = get_relative_position1();
+ //sprintf( txbuff, "p%d%s%d,%d%c", i + 1, tracksID[cars[i].trackID], cars[i].nlap, rpos, EOL );
+ sprintf( txbuff, "p%d%s%d,%d", 1, 1, loop1, rpos );
+ Serial.println( txbuff ); 
+ rpos = get_relative_position2();
+ sprintf( txbuff, "p%d%s%d,%d", 2, 1, loop2, rpos );
+ Serial.println( txbuff );    
+}
+
+
+
 
 void burning1(){
 //to do
@@ -239,25 +294,24 @@ int display_lcd_time(){
   //to do  
 }
 
-
-
 void draw_car1(void){for(int i=0;i<=loop1;i++){track.setPixelColor(((word)dist1 % NPIXELS)+i, track.COLOR1);};                   
-  }
+ }
 
 void draw_car2(void){for(int i=0;i<=loop2;i++){track.setPixelColor(((word)dist2 % NPIXELS)+i, track.COLOR2);};            
  }
   
-void loop() {
-    //for(int i=0;i<NPIXELS;i++){track.setPixelColor(i, track.Color(0,0,0));};
-    for(int i=0;i<NPIXELS;i++){track.setPixelColor(i, track.Color(0,0,(127-gravity_map[i])/8) );};
+void loop() {   
+    for(int i=0;i<NPIXELS;i++){track.setPixelColor(i, track.Color(0,0,0));};
+    if ((ENABLE_RAMP==1) && ( VIEW_RAMP==1)) {for(int i=0;i<(MED_RAMP-INI_RAMP);i++){track.setPixelColor(INI_RAMP+i, track.Color(24+i*4,0,24+i*4) );};  
+                                              for(int i=0;i<(END_RAMP-MED_RAMP);i++){track.setPixelColor(END_RAMP-i, track.Color(24+i*4,0,24+i*4) );};                                           
+                                             };    
     
     if ( (flag_sw1==1) && (digitalRead(PIN_P1)==0) ) {flag_sw1=0;speed1+=ACEL;};
     if ( (flag_sw1==0) && (digitalRead(PIN_P1)==1) ) {flag_sw1=1;};
 
     if ((gravity_map[(word)dist1 % NPIXELS])<127) speed1-=kg*(127-(gravity_map[(word)dist1 % NPIXELS]));
     if ((gravity_map[(word)dist1 % NPIXELS])>127) speed1+=kg*((gravity_map[(word)dist1 % NPIXELS])-127);
-    
-    
+        
     speed1-=speed1*kf; 
     
     if ( (flag_sw2==1) && (digitalRead(PIN_P2)==0) ) {flag_sw2=0;speed2+=ACEL;};
@@ -282,25 +336,36 @@ void loop() {
     if (dist2>NPIXELS*loop2) {loop2++;TBEEP=10;FBEEP=440*2;};
 
     if (loop1>loop_max) {Serial.println("w1");
-                         for(int i=0;i<NPIXELS/10;i++){track.setPixelColor(i,track.COLOR1);}; track.show();
+                         for(int i=0;i<NPIXELS/10;i++){track.setPixelColor(i,track.COLOR1_tail);}; track.show();
                          winner_fx(1);loop1=0;loop2=0;dist1=0;dist2=0;speed1=0;speed2=0;timestamp=0;
                          start_race();
                         }
     if (loop2>loop_max) {Serial.println("w2");
-                         for(int i=0;i<NPIXELS/10;i++){track.setPixelColor(i, track.COLOR2);}; track.show();
+                         for(int i=0;i<NPIXELS/10;i++){track.setPixelColor(i, track.COLOR2_tail);}; track.show();
                          winner_fx(2);loop1=0;loop2=0;dist1=0;dist2=0;speed1=0;speed2=0;timestamp=0;
                          start_race();
                         }
-    if ((millis() & 512)==(512*draworder)) {if (draworder==0) {draworder=1;}
-                          else {draworder=0;}   
-                         }; 
-
-    if (draworder==0) {draw_car1();draw_car2();}
-        else {draw_car2();draw_car1();}   
+   
+     if ((millis() & 512)==(512*draworder)) {if (draworder==0) {draworder=1;}
+                          else {draworder=0;}
+                         };
+    if (abs(round(speed1*100))>abs(round(speed2*100))) {draworder=1;};
+    if (abs(round(speed2*100))>abs(round(speed1*100))) {draworder=0;};
+       
+    if ( draworder==0 ) {
+      draw_car1();
+      draw_car2();
+    }
+    else {
+      draw_car2();
+      draw_car1();
+    } 
                  
     track.show(); 
     if (SMOTOR==1) tone(PIN_AUDIO,FBEEP+int(speed1*440*2)+int(speed2*440*3));
     delay(tdelay);
     if (TBEEP>0) {TBEEP--;} else {FBEEP=0;};
+    cont_print++; 
+    if (cont_print>100) {print_cars_position();cont_print=0;}
          
 }
